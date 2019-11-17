8c17df7777510ffc8f6cea611184ac5b8518b59f
Chumachenko Roman <rom4ik730@gmail.com>
2019-11-17 18:30:35 +0200

    Small cleanup.

--- app/src/main/java/com/legion1900/mvvmnews/views/NewsfeedActivity.kt
+++ app/src/main/java/com/legion1900/mvvmnews/views/NewsfeedActivity.kt
@@ -13,7 +13,6 @@ import androidx.lifecycle.ViewModelProviders
 import com.legion1900.mvvmnews.BuildConfig
 import com.legion1900.mvvmnews.R
 import com.legion1900.mvvmnews.databinding.ActivityNewsfeedBinding
-import com.legion1900.mvvmnews.models.data.Article
 import com.legion1900.mvvmnews.presenters.NewsPresentationModel
 import com.legion1900.mvvmnews.utils.dialogs.ErrorDialog
 import com.legion1900.mvvmnews.views.adapters.NewsAdapter
